f19580a992552d2a2416a9d51ddadf12beac179f
Brian Shaginaw <bshaginaw120@gmail.com>
2023-08-28 16:42:29 -0400

    Add more ranges

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,52 @@
-use std::ops::{Index, Range};
+use std::ops::*;
 
-#[derive(Debug)]
-pub struct MyRange<T> {
-    pub start: T,
-    pub end: T,
+// ===
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
+pub struct MyRange<Idx> {
+    pub start: RangeIndex<Idx>,
+    pub end: RangeIndex<Idx>,
 }
 
-impl MyRange<RangeIndex<usize>> {
+impl MyRange<usize> {
     pub fn for_slice_len(&self, len: usize) -> Range<usize> {
         self.start.for_slice_len(len)..self.end.for_slice_len(len)
     }
 }
 
-#[derive(Debug)]
-pub enum RangeIndex<T> {
-    FromFront(T),
-    FromBack(T),
+// ===
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
+pub struct MyRangeFrom<Idx> {
+    pub start: RangeIndex<Idx>,
+}
+
+impl MyRangeFrom<usize> {
+    pub fn for_slice_len(&self, len: usize) -> RangeFrom<usize> {
+        self.start.for_slice_len(len)..
+    }
+}
+
+// ===
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Hash)]
+pub struct MyRangeInclusive<Idx> {
+    pub start: RangeIndex<Idx>,
+    pub end: RangeIndex<Idx>,
+}
+
+impl MyRangeInclusive<usize> {
+    pub fn for_slice_len(&self, len: usize) -> RangeInclusive<usize> {
+        self.start.for_slice_len(len)..=self.end.for_slice_len(len)
+    }
+}
+
+// ===
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum RangeIndex<Idx> {
+    FromFront(Idx),
+    FromBack(Idx),
 }
 
 impl RangeIndex<usize> {
@@ -27,19 +58,28 @@ impl RangeIndex<usize> {
     }
 }
 
-impl<T> Index<MyRange<RangeIndex<usize>>> for Vec<T> {
+impl<Idx> Default for RangeIndex<Idx>
+where Idx: Default {
+    fn default() -> Self {
+        RangeIndex::FromFront(Default::default())
+    }
+}
+
+// ===
+
+impl<T> Index<MyRange<usize>> for Vec<T> {
     type Output = [T];
 
-    fn index(&self, rng: MyRange<RangeIndex<usize>>) -> &[T] {
+    fn index(&self, rng: MyRange<usize>) -> &[T] {
         let range = rng.for_slice_len(self.len());
         &self[range]
     }
 }
 
-impl<T> Index<MyRange<RangeIndex<usize>>> for [T] {
+impl<T> Index<MyRange<usize>> for [T] {
     type Output = [T];
 
-    fn index(&self, rng: MyRange<RangeIndex<usize>>) -> &[T] {
+    fn index(&self, rng: MyRange<usize>) -> &[T] {
         let range = rng.for_slice_len(self.len());
         &self[range]
     }
@@ -47,6 +87,9 @@ impl<T> Index<MyRange<RangeIndex<usize>>> for [T] {
 
 #[macro_export]
 macro_rules! range {
+    // we allow specifying a `range!(..)` for completeness, but..
+    // ..there's no need to create a custom type for it.
+    ( .. ) => { .. };
     ( $left:tt..$right:tt ) => { MyRange {
         start: RangeIndex::FromFront($left),
         end: RangeIndex::FromFront($right),
@@ -60,6 +103,28 @@ macro_rules! range {
         end: RangeIndex::FromBack($right),
     } };
     ( ^$left:tt..^$right:tt ) => { MyRange {
+        start: RangeIndex::FromBack($left),
+        end: RangeIndex::FromBack($right),
+    } };
+    ( $left:tt.. ) => { MyRangeFrom {
+        start: RangeIndex::FromFront($left),
+    } };
+    ( ^$left:tt.. ) => { MyRangeFrom {
+        start: RangeIndex::FromBack($left),
+    } };
+    ( $left:tt..=$right:tt ) => { MyRangeInclusive {
+        start: RangeIndex::FromFront($left),
+        end: RangeIndex::FromFront($right),
+    } };
+    ( ^$left:tt..=$right:tt ) => { MyRangeInclusive {
+        start: RangeIndex::FromBack($left),
+        end: RangeIndex::FromFront($right),
+    } };
+    ( $left:tt..=^$right:tt ) => { MyRangeInclusive {
+        start: RangeIndex::FromFront($left),
+        end: RangeIndex::FromBack($right),
+    } };
+    ( ^$left:tt..=^$right:tt ) => { MyRangeInclusive {
         start: RangeIndex::FromBack($left),
         end: RangeIndex::FromBack($right),
     } };
@@ -117,5 +182,26 @@ mod tests {
     fn test_macro() {
         let vec: Vec<_> = (0..10).collect();
         assert_eq!(vec[range!(2..^3)], [2, 3, 4, 5, 6]);
+    }
+
+    #[test]
+    fn test_homogeneous_macro_ranges() {
+        // exercises that all these ranges can go in the `vec` together,
+        // that we didn't cut corners and return a `Range` for `range!(2..7)`
+        let ranges = vec![
+            range!(2..7),
+            range!(2..^3),
+            range!(^8..7),
+            range!(^8..^3),
+        ];
+        let vec: Vec<_> = (0..10).collect();
+        for range in ranges {
+            assert_eq!(vec[range], [2, 3, 4, 5, 6]);
+        }
+    }
+
+    #[test]
+    fn test_full_range() {
+        assert_eq!(range!(..), ..);
     }
 }
