f8c56a2ec50752b6c892cca4ee0c4147e67ce81b
Brian Shaginaw <bshaginaw120@gmail.com>
2023-08-28 22:03:30 -0400

    Add a bunch of docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,57 @@
+//! Library for indexing and slicing from the back of a sequence.
+//!
+//! The main entrypoint for this library is the [`idx!`] macro, which
+//! generates the library's structs. These structs can be used to index
+//! into common sequence types, notably [`Vec`] and [`slice`].
+//! The [`idx!`] macro exposes a virtual unary operator `^`, which
+//! indicates the index should count from the back of the sequence
+//! instead of the front.
+//!
+//! # Examples
+//!
+//! ```rust
+//! # use from_back::idx;
+//! let vec = vec![8, 6, 7, 5, 3, 0, 9];
+//! // the element second from the back is 0
+//! assert_eq!(vec[idx!(^2)], 0);
+//! // slice the elements two from the front to three from the back (exclusive)
+//! assert_eq!(&vec[idx!(2..^3)], &[7, 5]);
+//! // slice the elements two from the front to three from the back (inclusive)
+//! assert_eq!(&vec[idx!(2..=^3)], &[7, 5, 3]);
+//! ```
+
 use std::ops::*;
 
 // ===
 
+/// Container type for indexing from the front or back of a sequence (`idx!(index)`)
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum SeqIndex<Idx> {
+    /// The index counts from the front of the sequence.
     FromFront(Idx),
+    /// The index counts from the back of the sequence.
     FromBack(Idx),
 }
 
 impl SeqIndex<usize> {
+    /// Convert this container to a native from-front [`usize`] for a sequence of the given `len`.
+    ///
+    /// If the value is `FromFront(index)`, returns `index`.
+    /// If the value is `FromBack(index)`, returns `len - index`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the from-back value exceeds the given length.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use from_back::idx;
+    /// let vec = vec![8, 6, 7, 5, 3, 0, 9];
+    /// let index = idx!(^2).for_seq_len(vec.len());
+    /// assert_eq!(index, 5);
+    /// assert_eq!(vec.get(index), Some(&0));
+    /// ```
     pub fn for_seq_len(&self, len: usize) -> usize {
         match self {
             &SeqIndex::FromFront(idx) => idx,
@@ -26,13 +69,49 @@ where Idx: Default {
 
 // ===
 
+/// A parallel to [`std::ops::Range`] (`idx!(start..end)`)
+///
+/// # Examples
+/// ```rust
+/// # use from_back::idx;
+/// let vec = vec![8, 6, 7, 5, 3, 0, 9];
+///
+/// // slice the elements two from the front to three from the back
+/// assert_eq!(&vec[idx!(2..^3)], &[7, 5]);
+///
+/// // slice the elements five from the back to four from the front
+/// assert_eq!(&vec[idx!(^5..4)], &[7, 5]);
+///
+/// // slice the elements two from the back to zero from the back (the back itself)
+/// assert_eq!(&vec[idx!(^2..^0)], &[0, 9]);
+/// ```
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash)]
 pub struct SeqRange<Idx> {
+    /// The lower bound of the range (inclusive).
     pub start: SeqIndex<Idx>,
+    /// The upper bound of the range (exclusive).
     pub end: SeqIndex<Idx>,
 }
 
 impl SeqRange<usize> {
+    /// Convert this container to a native [`Range`].
+    ///
+    /// Delegates to each index's [`SeqIndex::for_seq_len`].
+    ///
+    /// # Panics
+    ///
+    /// Panics if either index's conversion panics.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use from_back::idx;
+    /// let vec = vec![8, 6, 7, 5, 3, 0, 9];
+    /// let range = idx!(2..^2).for_seq_len(vec.len());
+    /// let expected: &[_] = &[7, 5, 3];
+    /// assert_eq!(range, 2..5);
+    /// assert_eq!(vec.get(range), Some(expected));
+    /// ```
     pub fn for_seq_len(&self, len: usize) -> Range<usize> {
         self.start.for_seq_len(len)..self.end.for_seq_len(len)
     }
@@ -40,12 +119,41 @@ impl SeqRange<usize> {
 
 // ===
 
+/// A parallel to [`std::ops::RangeFrom`] (`idx!(start..)`)
+///
+/// # Examples
+/// ```rust
+/// # use from_back::idx;
+/// let vec = vec![8, 6, 7, 5, 3, 0, 9];
+///
+/// // slice the elements starting two from the back
+/// assert_eq!(&vec[idx!(^2..)], &[0, 9]);
+/// ```
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash)]
 pub struct SeqRangeFrom<Idx> {
+    /// The lower bound of the range (inclusive).
     pub start: SeqIndex<Idx>,
 }
 
 impl SeqRangeFrom<usize> {
+    /// Convert this container to a native [`RangeFrom`].
+    ///
+    /// Delegates to the `start` index's [`SeqIndex::for_seq_len`].
+    ///
+    /// # Panics
+    ///
+    /// Panics if the `start` index's conversion panics.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use from_back::idx;
+    /// let vec = vec![8, 6, 7, 5, 3, 0, 9];
+    /// let range = idx!(^2..).for_seq_len(vec.len());
+    /// let expected: &[_] = &[0, 9];
+    /// assert_eq!(range, 5..);
+    /// assert_eq!(vec.get(range), Some(expected));
+    /// ```
     pub fn for_seq_len(&self, len: usize) -> RangeFrom<usize> {
         self.start.for_seq_len(len)..
     }
@@ -53,13 +161,49 @@ impl SeqRangeFrom<usize> {
 
 // ===
 
+/// A parallel to [`std::ops::RangeInclusive`] (`idx!(start..=end)`)
+///
+/// # Examples
+/// ```rust
+/// # use from_back::idx;
+/// let vec = vec![8, 6, 7, 5, 3, 0, 9];
+///
+/// // slice the elements two from the front to three from the back, inclusive
+/// assert_eq!(&vec[idx!(2..=^3)], &[7, 5, 3]);
+///
+/// // slice the elements five from the back to four from the front, inclusive
+/// assert_eq!(&vec[idx!(^5..=4)], &[7, 5, 3]);
+///
+/// // slice the elements two from the back to one from the back, inclusive
+/// assert_eq!(&vec[idx!(^2..=^1)], &[0, 9]);
+/// ```
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash)]
 pub struct SeqRangeInclusive<Idx> {
+    /// The lower bound of the range (inclusive).
     pub start: SeqIndex<Idx>,
+    /// The upper bound of the range (inclusive).
     pub end: SeqIndex<Idx>,
 }
 
 impl SeqRangeInclusive<usize> {
+    /// Convert this container to a native [`RangeInclusive`].
+    ///
+    /// Delegates to each index's [`SeqIndex::for_seq_len`].
+    ///
+    /// # Panics
+    ///
+    /// Panics if either index's conversion panics.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use from_back::idx;
+    /// let vec = vec![8, 6, 7, 5, 3, 0, 9];
+    /// let range = idx!(2..=^2).for_seq_len(vec.len());
+    /// let expected: &[_] = &[7, 5, 3, 0];
+    /// assert_eq!(range, 2..=5);
+    /// assert_eq!(vec.get(range), Some(expected));
+    /// ```
     pub fn for_seq_len(&self, len: usize) -> RangeInclusive<usize> {
         self.start.for_seq_len(len)..=self.end.for_seq_len(len)
     }
@@ -205,6 +349,26 @@ impl Index<SeqRangeInclusive<usize>> for String {
 
 // ===
 
+/// Create an index or range which may have "from back" components.
+///
+/// Generates structs which can be used to index
+/// into common sequence types, notably [`Vec`] and [`slice`].
+/// This macro exposes a virtual unary operator `^`, which
+/// indicates the index should count from the back of the sequence
+/// instead of the front.
+///
+/// # Examples
+///
+/// ```rust
+/// # use from_back::idx;
+/// let vec = vec![8, 6, 7, 5, 3, 0, 9];
+/// // the element second from the back is 0
+/// assert_eq!(vec[idx!(^2)], 0);
+/// // slice the elements two from the front to three from the back (exclusive)
+/// assert_eq!(&vec[idx!(2..^3)], &[7, 5]);
+/// // slice the elements two from the front to three from the back (inclusive)
+/// assert_eq!(&vec[idx!(2..=^3)], &[7, 5, 3]);
+/// ```
 #[macro_export]
 macro_rules! idx {
     // we allow specifying a `idx!(..)` for completeness, but..
